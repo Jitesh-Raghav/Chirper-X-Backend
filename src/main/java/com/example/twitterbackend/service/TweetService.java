db690f3a75adbc31aa9aff7c10c91e85f3110d5c
Jitesh Kumar Raghav <jkr18042001@gmail.com>
2024-03-21 00:13:39 +0530

    working on Tweet Service implementation

--- src/main/java/com/example/twitterbackend/service/TweetService.java
+++ src/main/java/com/example/twitterbackend/service/TweetService.java
@@ -1,5 +1,19 @@
 package com.example.twitterbackend.service;
 
+import com.example.twitterbackend.entity.Tweet;
+import com.example.twitterbackend.entity.User;
+import com.example.twitterbackend.exception.TweetException;
+import com.example.twitterbackend.exception.UserException;
+
+import java.util.List;
+
 public interface TweetService {
+
+    public Tweet createTweet(Tweet req, User user) throws UserException;
+    public List<Tweet> findAllTweet();
+    public Tweet retweet(Long tweetId, User user) throws UserException, TweetException;
+    public Tweet findById(Long TweetId) throws TweetException;
+    public void deleteTweetById(Long tweetId, Long userId) throws UserException, TweetException;
+    public Tweet removeFromRetweet(Long tweetId, User user) throws UserException, TweetException;
 
 }
