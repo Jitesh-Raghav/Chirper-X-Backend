a055bf3555aeedbc79dc25ac04150de8ee64bf00
Jitesh Kumar Raghav <jkr18042001@gmail.com>
2024-03-29 00:19:13 +0530

    implemented like Service methods

--- src/main/java/com/example/twitterbackend/service/LikeService.java
+++ src/main/java/com/example/twitterbackend/service/LikeService.java
@@ -1,4 +1,16 @@
 package com.example.twitterbackend.service;
 
+import com.example.twitterbackend.entity.Like;
+import com.example.twitterbackend.entity.User;
+import com.example.twitterbackend.exception.TweetException;
+import com.example.twitterbackend.exception.UserException;
+
+import java.util.List;
+
 public interface LikeService {
+
+    public Like likeTweet(Long tweetId, User user) throws UserException, TweetException;
+
+    public List<Like> getAllLikes(Long tweetId) throws TweetException;
+
 }
