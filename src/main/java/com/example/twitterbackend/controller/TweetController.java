6bfa3155e1f6b46d3ba9158e2aad792cf7920806
Jitesh Kumar Raghav <jkr18042001@gmail.com>
2024-03-29 23:22:09 +0530

    created all dto's, model mapper pending

--- src/main/java/com/example/twitterbackend/controller/TweetController.java
+++ src/main/java/com/example/twitterbackend/controller/TweetController.java
@@ -1,5 +1,8 @@
 package com.example.twitterbackend.controller;
 
+import com.example.twitterbackend.service.TweetService;
+import com.example.twitterbackend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/tweets")
 public class TweetController {
+
+    @Autowired
+    private TweetService tweetService;
+    @Autowired
+    private UserService userService;
+
+
 }
---
+++ src/main/java/com/example/twitterbackend/dto/mapper/UserDtoMapper.java
@@ -0,0 +1,5 @@
+package com.example.twitterbackend.dto.mapper;
+
+public class UserDtoMapper {
+
+}
