0a10a12c3a88b210dc35e1d79327befe9ee07606
Jitesh Kumar Raghav <jkr18042001@gmail.com>
2024-04-04 00:07:28 +0530

    done with user controller

---
+++ src/main/java/com/example/twitterbackend/util/UserUtil.java
@@ -0,0 +1,14 @@
+package com.example.twitterbackend.util;
+
+import com.example.twitterbackend.entity.User;
+
+public class UserUtil {
+
+    public static final boolean isReqUser(User reqUser, User user2){
+        return reqUser.getId().equals(user2.getId());
+    }
+
+    public static final boolean isFollowedByReqUser(User reqUser, User user2){
+        return reqUser.getFollowing().contains(user2);
+    }
+}
